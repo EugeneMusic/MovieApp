2036e5c6d46d634ad2bcf8854482e4de1d613986
EugeneMusic <muzychenko_eugene@mail.ru>
2022-02-08 22:36:37 +0300

    feat: add getData and showData func

--- main.js
+++ main.js
@@ -1,19 +1,59 @@
-const url ='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6b6e3d9b89131c14e442fabf8b936c2c';
+const url = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6b6e3d9b89131c14e442fabf8b936c2c';
 
 async function getData() {
     const res = await fetch(url);
     const data = await res.json();
     console.log(data);
     showData(data);
-  }
-  getData();
-  const main = document.querySelector('.main');
+}
+getData();
 
-  function showData(data) {
-    const img = document.createElement('img');
-    img.classList.add('poster-img');
-    img.src = "https://image.tmdb.org/t/p/w1280"+`${data.results[0].poster_path}`;
-    img.alt = `image`;
-    console.log(img.src)
-    main.append(img);
-  }
+const main = document.querySelector('.main');
+
+function showData(data) {
+    data.results.map((element, index) => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const info = document.createElement('div');
+        info.classList.add('info');
+
+        const name = document.createElement('h3');
+        name.classList.add('name');
+        name.textContent = `${data.results[index].original_title}`;
+
+        const rate = document.createElement('span');
+        rate.classList.add('rate');
+        rate.textContent = `${data.results[index].vote_average}`;
+        let rating = rate.textContent;
+        if (+rating < 5) {
+            rate.classList.add('red');
+        }
+        if (+rating >= 5 && +rating < 8) {
+            rate.classList.add('orange');
+        }
+        if (+rating >= 8) {
+            rate.classList.add('green');
+        }
+
+        const overviewTitle = document.createElement('div');
+        overviewTitle.textContent = `${data.results[index].original_title}`;
+
+        const overview = document.createElement('div');
+        overview.classList.add('overview');
+
+        const img = document.createElement('img');
+        img.classList.add('poster-img');
+        img.src = "https://image.tmdb.org/t/p/w1280" + `${data.results[index].poster_path}`;
+        img.alt = `image`;
+
+        console.log(img.src)
+        main.append(card);
+        card.append(img);
+        card.append(info);
+        card.append(overview);
+        info.append(name);
+        info.append(rate);
+        overview.append(overviewTitle, `${data.results[index].overview}`);
+    })
+}
